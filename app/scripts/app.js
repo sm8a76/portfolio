99b23c1b2a33c51d3bce3001a5a2ce978ac45f33
Sergio Mendoza <sm8a76@gmail.com>
2016-01-22 19:15:24 -0600

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolioApp
+ * @description
+ * # portfolioApp
+ *
+ * Main module of the application.
+ */
+/*angular
+  .module('portfolioApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });*/
+angular.module('portfolioApp', ['ui.router','ngResource'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+                    // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
+                }
+            })
+                    // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl:  'views/aboutus.html',
+                        controller  : 'AboutController'
+                   }
+                }
+            })
+                    // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'
+                    }
+                }
+            })
+
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
+            $urlRouterProvider.otherwise('/');
+    })
+;        
